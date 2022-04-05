551359029d061f139e99d563895a4d4f185b350c
Sifortin <33554200+Sifortin@users.noreply.github.com>
2022-04-05 23:19:13 +0200

    Revert "Revert "Added ServeStaticModule""
    This reverts commit d744ae425f23aea6998937936ec919b81812587c.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,14 @@ import { AppService } from './app.service';
 import { TwitterStrategy } from './twitter.strategy';
 import { ConfigModule } from '@nestjs/config';
 import { TscpModule } from './tscp/tscp.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'client/public'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
