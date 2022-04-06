f2311f937449569c3429dd3831462d76b32f75eb
thiryr <renaud.thiry.99@gmail.com>
2022-04-06 20:30:29 +0200

    Switching to lawyer-friendly logo

--- client/src/containers/NavBar/NavBar.tsx
+++ client/src/containers/NavBar/NavBar.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { NavBarS, NavLogo} from "./styles";
 import SearchBar from "./Components/SearchBar";
 import DropdownMenu from "./Components/Dropdown";
-import logo from "../../assets/Tweetter_logo.png";
+import logo from "../../assets/BirdLogo.png";
 
 
 export default function NavBar ()  {
     return(
             <nav>
                 <NavBarS>
-                    <NavLogo src={logo} alt="Tweeter logo" height={70} width={70}/>
+                    <NavLogo src={logo} alt="bird logo" height={70} width={70}/>
                     <SearchBar/>
                     <DropdownMenu/>
                 </NavBarS>  
