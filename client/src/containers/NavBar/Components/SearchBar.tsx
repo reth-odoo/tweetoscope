db8bf0630152286f46d1b9c724966f43df05df42
madessy <marien.dessy@student.unamur.be>
2022-04-15 21:15:57 +0200

    trying to merge my branch with dev

--- client/src/containers/NavBar/Components/SearchBar.tsx
+++ client/src/containers/NavBar/Components/SearchBar.tsx
@@ -1,12 +1,26 @@
 import React, {useState} from "react";// le useState est là pour quand on va le rattacher au vrai donnée.
 import {SearchBarForm, SearchBarButton, SearBarInput, SearchBarContainer, SearchBarInputContainer } from "../styles";
 
-function SearchBar() {
+function SearchBar({searchQuery, setSearchquery}: any ) {
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const filterPosts = (posts: any, query:any) => {
+        if (!query) {
+            return posts;
+        }
+    
+        return posts.filter((post: any) => {
+            const postName = post.name.toLowerCase();
+            return postName.includes(query);
+        });
+    };
     return(
         <SearchBarContainer>
             <SearchBarInputContainer>
-                <SearchBarForm id="searchbarinput">
+                <SearchBarForm id="searchbarinput" action="/" method="get">
                   <SearBarInput
+                  value={searchQuery}
+                  onInput= {e => setSearchquery(e.target)}
                   type="search"
                   name="searchbar"
                   id="searchbar"
--- client/src/containers/NavBar/NavBar.tsx
+++ client/src/containers/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState} from "react";
 import { NavBarS,NavLogoContainer, NavLogo} from "./styles";
 import SearchBar from "./Components/SearchBar";
 import DropdownMenu from "./Components/Dropdown";
@@ -6,16 +6,25 @@ import logo from "../../assets/BirdLogo.png";
 
 
 export default function NavBar ()  {
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const [searchQuery, setSearchQuery] = useState(query || '');
+    const filteredPosts = filterPosts(posts, searchQuery);
+
     return(
             <nav>
                 <NavBarS>
                     <NavLogoContainer>
                         <NavLogo src={logo} alt="bird logo"/>
                     </NavLogoContainer>
-                    <SearchBar/>
+                    <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
                     <DropdownMenu/>
                 </NavBarS>  
             </nav>
         
     );
 }
+function posts(posts: any, searchQuery: string) {
+    throw new Error("Function not implemented.");
+}
+
