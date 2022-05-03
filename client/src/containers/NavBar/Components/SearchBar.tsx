c1a6d12c805398667e28d1fbc0f0580741f51638
madessy <marien.dessy@student.unamur.be>
2022-05-03 18:17:46 +0200

    reparer les prob de merge + searchbar

--- client/src/containers/NavBar/Components/SearchBar.tsx
+++ client/src/containers/NavBar/Components/SearchBar.tsx
@@ -1,15 +1,58 @@
-import React from "react";
-import { SearBarS } from "../styles";
+import React, {useState} from "react";// le useState est là pour quand on va le rattacher au vrai donnée.
+import {SearchBarForm, SearchBarButton, SearBarInput, SearchBarContainer, 
+    SearchBarInputContainer, DataListContainer, DataList, DataListItem } from "../styles";
 
-export default function SearchBar() {
+function SearchBar(data: any) {
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+    
+    const handleFilter = (event : any) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+        const newFilter = data.filter((value: { title: string; }) => {
+            return value.title.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if (searchWord === "") {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        }
+  };
+
     return(
-        <>
-            <SearBarS
-            type="search"
-            name="searchbar"
-            id="searchbar"
-            placeholder="Rechercher"
-            />
-        </>
+        <SearchBarContainer>
+            <SearchBarInputContainer>
+                <SearchBarForm id="searchbarinput" action="/" method="get">
+                  <SearBarInput
+                  value={wordEntered}
+                  type="search"
+                  name="searchbar"
+                  id="searchbar"
+                  placeholder="Search"
+                  onChange={handleFilter}
+                  />
+                </SearchBarForm>
+                <SearchBarButton type="submit" form="searchbarinput">Search</SearchBarButton>
+            </SearchBarInputContainer>     
+        
+            
+    </SearchBarContainer>
     );
 }
+
+export default SearchBar
+
+/*{filteredData.length != 0 && (
+    <DataListContainer>
+        {filteredData.slice(0,15).map((value, key) => {
+            return (
+                <DataList>
+                    <DataListItem href={value.link} target="_blank"> 
+                        <p>{value.title} </p>
+                    </DataListItem> 
+                </DataList>
+            );
+        })}
+    </DataListContainer>
+)}*/
